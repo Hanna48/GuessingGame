e149b72098ef482cbf8410fe71337eff4806901f
hanna.chung35 <hanna.chung35@TI03NB59>
2019-04-01 21:30:01 +0800

    add bootstrap card

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <H1 class="titleStyle">MyToDoList</H1>
+    <MyList></MyList>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyList from "./components/MyList.vue"
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MyList,
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +24,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.titleStyle {
+  margin-top: 20px;
+  color: #449b94;
+  text-align: center;
 }
 </style>
