5633d5781ac2898ea4680dcf88607957b679ca29
hanna.chung35 <hanna.chung35@TI03NB59>
2019-04-02 22:00:33 +0800

    Desktop ToDoList interface

--- src/components/MyList.vue
+++ src/components/MyList.vue
@@ -1,26 +1,33 @@
 <template>
-  <div>
-    <b-card
-      title="Card Title"
-    img-src="https://picsum.photos/600/300/?image=25"
-    img-alt="Image"
-    img-top
-    tag="article"
-    style="max-width: 20rem;"
-    class="mb-2"
-  >
-    <b-card-text>
-      Some quick example text to build on the card title and make up the bulk of the card's content.
-    </b-card-text>
-
-    <b-button href="#" variant="primary">Go somewhere</b-button>
+  <div class="cardStyle">
+    <b-card class="mb-2">
+      <b-row md="4">
+        <b-col md="9">
+          <button href="#" class="stateBtnStyle btnText">All</button>
+          <button href="#" class="stateBtnStyle btnText">Unfinished</button>
+          <button href="#" class="stateBtnStyle btnText">Completed</button>
+        </b-col>
+        <b-col md="3" style="margin: auto">
+          <button href="#" class="itemBtnStyle btnText">Add</button>
+          <button href="#" class="itemBtnStyle btnText">Edit</button>
+          <button href="#" class="itemBtnStyle btnText">Delete</button>
+        </b-col>
+      </b-row>
+      <b-row md="8">
+        <input class="inputStyle">
+        <input class="inputStyle">
+        <input class="inputStyle">
+        <input class="inputStyle">
+        <input class="inputStyle">
+        <input class="inputStyle">
+      </b-row>
     </b-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'MyList',
+  name: "MyList",
   props: {
     msg: String
   }
@@ -29,18 +36,49 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.cardStyle {
+  max-width: 95%;
+  margin-left: 31px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.stateBtnStyle {
+  height: 45px;
+  width: 100px;
+  margin-right: 15px;
+  -webkit-border-radius: 28;
+  -moz-border-radius: 28;
+  border-radius: 28px;
+  background-color: #3da19f;
+  border: 0px;
+  outline: none;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.stateBtnStyle:hover {
+  background-color: #3da19f;
 }
-a {
-  color: #42b983;
+.stateBtnStyle:active {
+  background-color: #a2cd85;
+}
+.itemBtnStyle {
+  height: 30px;
+  width: 60px;
+  margin-right: 5px;
+  -webkit-border-radius: 5;
+  -moz-border-radius: 5;
+  border-radius: 5px;
+  vertical-align: 0%;
+  background-color: #a2cd85;
+  border: 0px;
+  outline: none;
+}
+.btnText {
+  font-family: "Noto Sans TC", "Avenir", Helvetica, Arial, sans-serif;
+  color: #fff;
+}
+.inputStyle {
+  width: 100%;
+  margin-top: 15px;
+  border: 0;
+  outline: none;
+  background: transparent;
+  border-bottom: 0.8px solid #dbdbdb;
 }
 </style>
