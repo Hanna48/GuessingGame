44627e1e029f55ca1f932bed7556e118f5a83129
hanna.chung35 <hanna.chung35@TI03NB59>
2019-04-09 00:35:52 +0800

    fixed vueX structure and add todo's id

--- src/store.js
+++ src/store.js
@@ -3,14 +3,90 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store ({
-    state:{
+export default new Vuex.Store({
 
+    state: {
+        todos: [],
+        newTodo: "",
+        isShowUnfinish: true,
+        isShowComplete: true,
+    },
+    getters: {
+        newTodo: state => state.newTodo,
+        todos: state => state.todos.filter((todo) => { return !todo.iscompleted }),
+        completed: state => state.todos.filter((todo) => { return todo.iscompleted }),
     },
     mutations: {
-
+        GET_TODO(state, todo) {
+            state.newTodo = todo
+        },
+        ADD_TODO(state) {
+            let todoKey = state.todos.length;
+            state.todos.push({
+                id: todoKey,
+                body: state.newTodo,
+                iscompleted: false,
+            })
+            todoKey++;
+        },
+        CLEAR_TODO(state) {
+            state.newTodo = ""
+        },
+        COMPLETE_TODO(state, todo) {
+            state.todos.forEach(x => {
+                if (x.id == todo.id) {
+                    x.iscompleted = true;
+                }
+            })
+        },
+        EDIT_TODO(state, todo) {
+            var todos = state.todos
+            todos.splice(todos.indexOf(todo), 1)
+            state.todos = todos
+            state.newTodo = todo.body
+        },
+        DELETE_TODO(state, todo) {
+            var todos = state.todos
+            todos.splice(todos.indexOf(todo), 1)
+        },
+        SHOW_COMPLETE(state) {
+            state.isShowComplete = !state.isShowComplete;
+        },
+        SHOW_UNFINISH(state) {
+            state.isShowUnfinish = !state.isShowUnfinish;
+        },
+        SHOW_ALL(state) {
+            state.isShowUnfinish = true;
+            state.isShowComplete = true;
+        }
     },
     actions: {
-
+        GetTodo({ commit }, todo) {
+            commit('GET_TODO', todo)
+        },
+        AddTodo({ commit }) {
+            commit("ADD_TODO");
+        },
+        ClearTodo({ commit }) {
+            commit('CLEAR_TODO')
+        },
+        CompleteTodo({ commit }, todo) {
+            commit('COMPLETE_TODO', todo)
+        },
+        EditTodo({ commit }, todo) {
+            commit('EDIT_TODO', todo)
+        },
+        DeleteTodo({ commit }, todo) {
+            commit('DELETE_TODO', todo);
+        },
+        ShowComplete({ commit }) {
+            commit('SHOW_COMPLETE');
+        },
+        ShowUnfinish({ commit }) {
+            commit('SHOW_UNFINISH');
+        },
+        ShowAll({ commit }) {
+            commit('SHOW_ALL');
+        }
     },
 })
